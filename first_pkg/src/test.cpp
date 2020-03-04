cec0fd7fd78d8c7520ce0521a61d25a36a6f244f
Bennctu <physical31031@gmail.com>
2020-03-04 15:51:42 +0800

    blank

--- first_pkg/src/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <ros/ros.h>			//include ros library
-int main(int argc, char **argv)
-{
-ros::init(argc, argv, "test");     	//NODE name
-ros::NodeHandle n;	  			 //NodeHandle n: start node 
-ROS_INFO("Hello world!");		//print
-}
-
